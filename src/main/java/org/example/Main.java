df2ddcd6284849410307e3c9cdceea025c2a49c7
Madumarov Davron <menaceteacher@gmail.com>
2025-01-22 11:19:09 +0500

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static boolean isPalindrome(String text) {
         String cleanText = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
@@ -10,5 +12,12 @@ public class Main {
     }
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Palindrome tekshiruvchi dastur");
+        System.out.print("So'z yoki jumlani kiriting: ");
+        String input = scanner.nextLine();
+        //Pass uchun
+        //o'sha metodi o'ziz yozin
     }
 }
