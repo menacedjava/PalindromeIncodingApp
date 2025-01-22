1a104641b08ac7452aff715f5028db6cad8ecabe
Madumarov Davron <menaceteacher@gmail.com>
2025-01-22 11:18:22 +0500

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static boolean isPalindrome(String text) {
         String cleanText = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-
+        String reversedText = new StringBuilder(cleanText).reverse().toString();
+        return cleanText.equals(reversedText);
     }
     public static void main(String[] args) {
 
