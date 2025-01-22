7e31d646ca2f06ee413ac66628b3f8747e988c44
Madumarov Davron <menaceteacher@gmail.com>
2025-01-22 11:18:04 +0500

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,11 @@ package org.example;
 
 
 public class Main {
+    public static boolean isPalindrome(String text) {
+        String cleanText = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
+
+    }
     public static void main(String[] args) {
 
     }
